b699ea6635a2a80cd45199601b8e9cbc63d63a9f
Andres Fernando <andres.fernando08@gmail.com>
2021-10-17 16:00:20 -0500

    [add] pm2 web

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,5 +7,12 @@ module.exports = {
 			error_file: '_error.log',
 			out_file: '_out.log',
 		},
+		{
+			name: 'wordapp-web',
+			script: 'npm run dev:serve',
+			log_date_format: 'YYYY-MM-DD HH:mm Z',
+			error_file: '_error_web.log',
+			out_file: '_out_web.log',
+		},
 	],
 };
